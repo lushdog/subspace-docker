65e4abdd9b92c1e8f9124e21ce6f12b4ac7eba8a
HoraceZhang <impressioncr@hotmail.com>
2023-09-27 16:25:48 +0800

    添加farmer

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # subspace-docker
 Dockerfile for subspace
+
+## farmer
+
+farmer连接远程的node，所以可以只单独运行farmer程序。
+
+### 构建
+
+`docker build . -f Dockerfile.farm --tag subspace-farmer --no-cache`
+
+### 运行
+
+`docker compose up -d farmer`
---
+++ download.mjs
@@ -0,0 +1,28 @@
+const RELEASES_URL =
+  'https://api.github.com/repos/subspace/subspace/releases/latest'
+
+const IS_ARM = process.env.IS_ARM === 'true'
+const IS_SKYLAKE = process.env.IS_SKYLAKE
+  ? process.env.IS_SKYLAKE === 'true'
+  : true
+const downloadType = process.argv[3]
+const reg = IS_ARM
+  ? /ubuntu-aarch64/
+  : IS_SKYLAKE
+  ? /ubuntu-x86_64-skylake/
+  : /ubuntu-x86_64-v2/
+const download = async () => {
+  const relases = await fetch(RELEASES_URL).then((res) => res.json())
+  const allRelases = relases.assets
+    .filter((item) => item.name.includes(downloadType))
+    .map((item) => ({
+      name: item.name,
+      browser_download_url: item.browser_download_url,
+    }))
+  const target = allRelases.find((rels) => reg.test(rels.name))
+  console.log(target)
+  await $`wget ${target.browser_download_url}`
+  await $`mv ${target.name} ${downloadType}`
+}
+
+await download()

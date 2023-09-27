6e28d676b3bbfaf9a214e847c7c61890f1da02fc
HoraceZhang <impressioncr@hotmail.com>
2023-09-27 17:15:17 +0800

    done

--- download.mjs
+++ download.mjs
@@ -1,3 +1,8 @@
+/**
+ * @description 解析最新版本，下载文件
+ * @author lushdog
+ */
+
 const RELEASES_URL =
   'https://api.github.com/repos/subspace/subspace/releases/latest'
 
@@ -5,12 +10,15 @@ const IS_ARM = process.env.IS_ARM === 'true'
 const IS_SKYLAKE = process.env.IS_SKYLAKE
   ? process.env.IS_SKYLAKE === 'true'
   : true
+
 const downloadType = process.argv[3]
+
 const reg = IS_ARM
   ? /ubuntu-aarch64/
   : IS_SKYLAKE
   ? /ubuntu-x86_64-skylake/
   : /ubuntu-x86_64-v2/
+  
 const download = async () => {
   const relases = await fetch(RELEASES_URL).then((res) => res.json())
   const allRelases = relases.assets
--- farmer.entrypoint.sh
+++ farmer.entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Below are default values, they can be overridden by environment variables
 REWARD_ADDRESS=${REWARD_ADDRESS}
 NODE_RPC_URL=${NODE_RPC_URL}
 PLOT_SIZE=${PLOT_SIZE}
 
-# Execute the main command with the parameters
 exec ./farmer farm --reward-address $REWARD_ADDRESS --node-rpc-url $NODE_RPC_URL path=/app/plot,size=$PLOT_SIZE "$@"

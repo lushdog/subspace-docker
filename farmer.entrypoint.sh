d2e2d026d0a28983d928434e2ef3bad4f83ff736
HoraceZhang <impressioncr@hotmail.com>
2024-11-07 13:57:16 +0800

    sleep farmer

--- farmer.entrypoint.sh
+++ farmer.entrypoint.sh
@@ -4,4 +4,8 @@ REWARD_ADDRESS=${REWARD_ADDRESS}
 NODE_RPC_URL=${NODE_RPC_URL}
 PLOT_SIZE=${PLOT_SIZE}
 
+sleep 15
+
+echo "starting farmer"
+
 exec ./farmer farm --reward-address $REWARD_ADDRESS --node-rpc-url $NODE_RPC_URL path=/app/plot,size=$PLOT_SIZE "$@"

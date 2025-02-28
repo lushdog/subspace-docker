31db8b4e16bfa3930ec179a4de23d0b8506b5516
HoraceZhang <impressioncr@hotmail.com>
2025-02-28 14:44:01 +0800

    修改镜像地址

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build . -f Dockerfile.farm --tag trancelife/subspace-farmer:latest --no-cache
-docker build . -f Dockerfile.node --tag trancelife/subspace-node:latest --no-cache
+docker build . -f Dockerfile.farm --tag ghcr.io/lushdog/subspace-farmer:latest --no-cache
+docker build . -f Dockerfile.node --tag ghcr.io/lushdog/subspace-node:latest --no-cache
 
 if docker info | grep "trancelife"; then
     echo "Found 'trancelife', executing 'docker push'..."
